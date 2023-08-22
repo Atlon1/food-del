595dff7c7ffe3be83f4b660f346a24c931b31598
Atlon1 <maciej.szajstek@gmail.com>
2023-08-22 20:40:33 +0200

    types on modal and pizza component

--- app/components/Pizza.tsx
+++ app/components/Pizza.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from "react";
+import React, {useState, FC} from "react";
 
 import Image from 'next/image'
 
@@ -7,13 +7,20 @@ import Modal from 'react-modal'
 
 import PizzaDetails from "@/app/components/PizzaDetails";
 
-import { IoCloseOutline } from 'react-icons/io5'
+import {IoCloseOutline} from 'react-icons/io5'
 
-const Pizza = ({pizza}) => {
-    console.log(pizza)
-    return <div>
-        <Image width={270} height={270} src={pizza.image} alt='pizza'/>
-    </div>;
+interface pizzaProps {
+    pizza : any
+}
+
+const Pizza: FC<pizzaProps> = ({pizza}) => {
+
+
+    return (
+        <div className='group py-2 px-4 xl:py-4 xl:px-2 rounded-xl'>
+            <Image width={270} height={270} src={pizza.image} alt='pizza' priority={true}/>
+        </div>
+    )
 };
 
 export default Pizza;
