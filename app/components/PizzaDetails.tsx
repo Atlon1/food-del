2da69f3a758587cde6f00b2a9089f2eefffefc8a
Atlon1 <maciej.szajstek@gmail.com>
2023-08-28 20:17:44 +0200

    section size selection started

--- app/components/PizzaDetails.tsx
+++ app/components/PizzaDetails.tsx
@@ -44,61 +44,62 @@ const PizzaDetails: FC<pizzaType> = ({pizza, modal, setModal}) => {
     }, [additionalTopping]);
 
 
-
     return (
-    <div className='flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8'>
-        <div className='lg:flex-1 flex justify-center items-center'>
-            <div className='max-w-[300px] lg:max-w-none mt-6 lg:mt-0'>
-                <Image
-                    width={450}
-                    height={450}
-                    src={pizza.image}
-                    alt=''
-                    priority={true}
-                    className='mx-auto relative'
-                />
+        <div className='flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8'>
+            <div className='lg:flex-1 flex justify-center items-center'>
+                <div className='max-w-[300px] lg:max-w-none mt-6 lg:mt-0'>
+                    <Image
+                        width={450}
+                        height={450}
+                        src={pizza.image}
+                        alt=''
+                        priority={true}
+                        className='mx-auto relative'
+                    />
+                </div>
             </div>
-        </div>
-        <div className='bg-pink-200 flex flex-col flex-1'>
-            <div className='flex-1 p-2 text-center lg:text-left'>
-                <div className='flex-1 bg-white overflow-y-scroll h-[26vh] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-white pr-2'>
-                    <div className='font-semibold'>
-                        <h2 className='capitalize text-3xl mb-1'>{pizza.name}</h2>
-                        <div className='bg-yellow-200 mb-6 text-lg font-medium'>
-                            <span>{ size === "small" ? '25 cm' :
+            <div className='bg-pink-200 flex flex-col flex-1'>
+                <div className='flex-1 p-2 text-center lg:text-left'>
+                    <div
+                        className='flex-1 bg-white overflow-y-scroll h-[26vh] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-white pr-2'>
+                        <div className='font-semibold'>
+                            <h2 className='capitalize text-3xl mb-1'>{pizza.name}</h2>
+                            <div className='bg-yellow-200 mb-6 text-lg font-medium'>
+                            <span>{size === "small" ? '25 cm' :
                                 size === "medium" ? "30 cm" :
                                     size === "large" ? "35 cm" : null}
                             </span>
-                            <span>
+                                <span>
                                 , {crust} crust
                             </span>
+                            </div>
+                        </div>
+                        <SizeSelection pizza={pizza} size={size} setSize={setSize}/>
+                        <CrustSelection/>
+                        <div>
+                            Choose topping
+                        </div>
+                        <div>
+                            {pizza.toppings?.map((topping: string, index: number) => {
+                                return <Topping key={index}/>
+                            })}
                         </div>
                     </div>
-                    <SizeSelection pizza={pizza} size={size} setSize={setSize}/>
-                    <CrustSelection/>
-                    <div>
-                        Choose topping
-                    </div>
-                    <div>
-                        {pizza.toppings?.map((topping: string, index: number)=> {
-                            return <Topping key={index}/>
-                        })}
-                    </div>
+                </div>
+                <div className='h-full flex items-center px-2 lg:items-end'>
+                    <button className=' btn btn-lg gradient w-full flex  justify-center'>
+                        <div>
+                            Add to cart for
+                        </div>
+                        <div>
+                            $ {price}
+                        </div>
+                    </button>
                 </div>
             </div>
-            <div className='h-full flex items-center px-2 lg:items-end'>
-                <button className=' btn btn-lg gradient w-full flex  justify-center'>
-                    <div>
-                        Add to cart for
-                    </div>
-                    <div>
-                        $ {price}
-                    </div>
-                </button>
-            </div>
         </div>
-    </div>
-    )};
+    )
+};
 
 export default PizzaDetails;
 
--- app/components/SizeSelection.tsx
+++ app/components/SizeSelection.tsx
@@ -2,7 +2,7 @@ import React, {FC} from "react";
 import Image from "next/image";
 
 interface SizeselcetionProps {
-    pizza: string
+    pizza: any
     size: any
     setSize: any
 }
@@ -11,9 +11,21 @@ const SizeSelection: FC<SizeselcetionProps> = ({pizza, size, setSize}) => {
 
 
     return (
-        <div className='bg-indigo-200'>
+        <div className=''>
             <div>
-
+                <label className='bg-yellow-200'>
+                    <Image
+                        className={`${size === 'small' ? 'border-2 border-orange p-[2px] rounded-full' : 'border-transparent filter saturate-[.1]'}`}
+                        src={pizza.image} width={60} height={60} alt='image'/>
+                    <input type='radio'
+                           name='size'
+                           value='small'
+                           checked={size === 'small'}
+                           onChange={(e)=> setSize(e.target.value)}
+                           className='appearance-none'
+                    />
+                    small
+                </label>
             </div>
         </div>);
 };
