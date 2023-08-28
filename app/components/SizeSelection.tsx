b92cbf9a788372040b1fd9e905ba4aff3404c7dc
Atlon1 <maciej.szajstek@gmail.com>
2023-08-28 20:28:49 +0200

    section size selection started

--- app/components/SizeSelection.tsx
+++ app/components/SizeSelection.tsx
@@ -13,7 +13,7 @@ const SizeSelection: FC<SizeselcetionProps> = ({pizza, size, setSize}) => {
     return (
         <div className=''>
             <div>
-                <label className='bg-yellow-200'>
+                <label className='flex flex-col items-center gap-x-2 cursor-pointer'>
                     <Image
                         className={`${size === 'small' ? 'border-2 border-orange p-[2px] rounded-full' : 'border-transparent filter saturate-[.1]'}`}
                         src={pizza.image} width={60} height={60} alt='image'/>
@@ -21,10 +21,36 @@ const SizeSelection: FC<SizeselcetionProps> = ({pizza, size, setSize}) => {
                            name='size'
                            value='small'
                            checked={size === 'small'}
-                           onChange={(e)=> setSize(e.target.value)}
+                           onChange={(e) => setSize(e.target.value)}
                            className='appearance-none'
                     />
                     small
+                </label>
+                <label className='flex flex-col items-center gap-x-2 cursor-pointer'>
+                    <Image
+                        className={`${size === 'medium' ? 'border-2 border-orange p-[2px] rounded-full' : 'border-transparent filter saturate-[.1]'}`}
+                        src={pizza.image} width={70} height={70} alt='image'/>
+                    <input type='radio'
+                           name='size'
+                           value='medium'
+                           checked={size === 'medium'}
+                           onChange={(e) => setSize(e.target.value)}
+                           className='appearance-none'
+                    />
+                    medium
+                </label>
+                <label className='flex flex-col items-center gap-x-2 cursor-pointer'>
+                    <Image
+                        className={`${size === 'large' ? 'border-2 border-orange p-[2px] rounded-full' : 'border-transparent filter saturate-[.1]'}`}
+                        src={pizza.image} width={80} height={80} alt='image'/>
+                    <input type='radio'
+                           name='size'
+                           value='large'
+                           checked={size === 'large'}
+                           onChange={(e) => setSize(e.target.value)}
+                           className='appearance-none'
+                    />
+                    Large
                 </label>
             </div>
         </div>);
