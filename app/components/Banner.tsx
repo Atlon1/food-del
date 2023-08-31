39eb5fea0e2c474cab2e6bf55c06ff39d242b201
Atlon1 <maciej.szajstek@gmail.com>
2023-08-31 21:02:11 +0200

    banner middle

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 import {MouseParallaxContainer, MouseParallaxChild} from 'react-parallax-mouse'
+import Image from "next/image";
 
 
 const Banner = () => {
@@ -14,14 +15,28 @@ const Banner = () => {
                 resetOnLeave
                 className='w-full flex flex-col lg:flex-row justify-between items-center'
             >
-                <MouseParallaxChild>
-                    <div>
+                <MouseParallaxChild
+                    factorX={0.1}
+                    factorY={0.2}
+                >
+                    <div className='flex flex-col lg:flex-row items-center text-center lg:text-left flex-1 px-6 text-white'>
                         <div>Best pizza in town</div>
-                        <h1 className='text-6xl lg:text-8xl font-bangers text-white'>Pizza perfection <br/> in every
+                        <h1 className='text-6xl lg:text-8xl font-bangers drop-shadow-mf'>Pizza perfection <br/> in every
                             bite</h1>
                     </div>
                 </MouseParallaxChild>
-                <MouseParallaxChild>image</MouseParallaxChild>
+                <MouseParallaxChild 
+                    factorX={0.2} 
+                    factorY={0.3} 
+                    className='relative'
+                >
+                    <div>
+                        <div>
+                            <Image src={'/pizza-banner.png'} alt="banner" width={550} height={558}/>
+                        </div>
+                    </div>
+                
+                </MouseParallaxChild>
             </MouseParallaxContainer>
         </div>
     </section>;
