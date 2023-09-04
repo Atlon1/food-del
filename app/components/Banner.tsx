02f87baa20be5ce4c906f27a9bbada52624de1a2
Atlon1 <maciej.szajstek@gmail.com>
2023-09-04 19:21:27 +0200

    banner rdy

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -36,8 +36,8 @@ const Banner = () => {
                     factorY={0.3}
                     className='relative'
                 >
-                    <div>
-                        <div>
+                    <div className='flex flex-col lg:flex-row items-center text-center lg:text-left flex-1 px-6'>
+                        <div className='flex-1 flex justify-end max-w-sm lg:max-w-max'>
                             <Image
                                 src={'/pizza-banner.png'}
                                 alt="banner"
@@ -48,9 +48,9 @@ const Banner = () => {
                         </div>
                     </div>
                     <MouseParallaxChild
-                    factorX={0.2}
-                    factorY={0.3}
-                    className='absolute top-6 left-4 hidden xl:flex'
+                        factorX={0.2}
+                        factorY={0.3}
+                        className='absolute top-6 left-4 hidden xl:flex'
                     >
                         <Image
                             src={`/chilli-1.png`}
@@ -59,6 +59,67 @@ const Banner = () => {
                             alt='chilli'
                             priority={true}/>
                     </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.4}
+                        factorY={0.4}
+                        className='absolute top-16 -left-4 hidden xl:flex'
+                    >
+                        <Image
+                            src={`/chilli-2.png`}
+                            width={160}
+                            height={84}
+                            alt='chilli'
+                            priority={true}/>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.6}
+                        factorY={0.6}
+                        className='absolute top-80 -left-24 hidden xl:flex'
+                    >
+                        <Image
+                            src={`/chilli-2.png`}
+                            width={84}
+                            height={72}
+                            alt='chilli'
+                            priority={true}/>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.3}
+                        factorY={0.6}
+                        className='absolute top-[22rem] -left-8 hidden xl:flex'
+                    >
+                        <Image
+                            src={`/garlic-2.png`}
+                            width={100}
+                            height={72}
+                            alt='chilli'
+                            priority={true}/>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.4}
+                        factorY={1}
+                        className='absolute top-96 -left-20 hidden xl:flex'
+                    >
+                        <Image
+                            src={`/garlic-3.png`}
+                            width={100}
+                            height={72}
+                            alt='chilli'
+                            priority={true}/>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.2}
+                        factorY={0.2}
+                        className='absolute top-96 left-12 hidden xl:flex'
+                    >
+                        <Image
+                            src={`/leaves.png`}
+                            width={180}
+                            height={72}
+                            alt='chilli'
+                            priority={true}/>
+                    </MouseParallaxChild>
+
                 </MouseParallaxChild>
             </MouseParallaxContainer>
         </div>
