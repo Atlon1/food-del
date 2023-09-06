7d037dd2054fd5e6d32e72efd2b36ef20b84f08a
Atlon1 <maciej.szajstek@gmail.com>
2023-09-06 21:58:59 +0200

    cart Top start

--- app/components/CartMobile.tsx
+++ app/components/CartMobile.tsx
@@ -3,6 +3,7 @@
 import {useContext} from "react";
 import {CartContext} from "@/app/context/CartContext";
 import CartTop from "@/app/components/CartTop";
+import CartBottom from "@/app/components/CartBottom";
 
 
 const CartMobile = () => {
@@ -12,6 +13,8 @@ const CartMobile = () => {
 
     return <div className={`${isOpen ? 'bottom-0' : '-bottom-full'} bg-white fixed w-full h-full left-0 z-20 transition-all duration-300 lg:hidden flex flex-col`}>
         <CartTop/>
+        <div>Cart Items</div>
+        <CartBottom/>
     </div>;
 };
 
--- app/components/CartTop.tsx
+++ app/components/CartTop.tsx
@@ -7,7 +7,8 @@ const CartTop = () => {
     const {setIsOpen} = useContext<any>(CartContext)
 
     return (
-        <div>
+        <div className='w-full h-20 border-b flex items-center justify-between px-10'>
+            <div className='font-semibold'>Shopping Bag(3)</div>
             <div onClick={()=> setIsOpen(false)}
             className='cursor-pointer group'
             >
