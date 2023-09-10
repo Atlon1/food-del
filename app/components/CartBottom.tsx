cb1a1598d886b0e183c25ba5a99ea80bd891b828
Atlon1 <maciej.szajstek@gmail.com>
2023-09-10 21:02:08 +0200

    section cart bottom prepare

--- app/components/CartBottom.tsx
+++ app/components/CartBottom.tsx
@@ -1,5 +1,35 @@
+import React, {useContext} from "react";
+
+import {IoCloseOutline} from 'react-icons/io5'
+
+import CheckoutDetails from "@/app/components/CheckoutDetails";
+
+import Modal from "react-modal";
+
+import {CartContext} from "@/app/context/CartContext";
+
+
 const CartBottom = () => {
-    return <div>CartBottom</div>;
+
+
+    const {setIsOpen, cart} = useContext<any>(CartContext)
+
+    return <>{cart.length >= 1 ? <div>
+            <div className='px-6 py-3 lg:py-6 mt-auto text-lg font-semibold font-robotoCondensed'>
+                <div>Total:</div>
+                <div>$320</div>
+            </div>
+            <div className='flex flex-col gap-y-3'>
+                <button className='btn btn-lg gradient font-semibold flex justify-center'>
+                    Checkout
+                </button>
+            </div>
+        </div> :
+        <div className='absolute top-0 w-full h-full flex justify-center items-center -z-10'>
+            <div>Your cart is empty</div>
+        </div>}
+
+    </>
 };
 
 export default CartBottom;
