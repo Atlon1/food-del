2d2e09319114f683fad475078be1b6c87266cdb5
Atlon1 <maciej.szajstek@gmail.com>
2023-09-14 22:01:53 +0200

    checkout preprere

--- app/components/CheckoutDetails.tsx
+++ app/components/CheckoutDetails.tsx
@@ -1,19 +1,53 @@
 import React, {FC, useContext} from "react";
-import {TFunc} from "ts-interface-checker";
-import Image from "next/image";
 import {CartContext} from "@/app/context/CartContext";
 
 
 interface DetailsProps {
     setModal: React.Dispatch<React.SetStateAction<any>>
 }
+
 const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
 
-    const {cart, setCart, cartTotal } = useContext<any>(CartContext)
+    const {cart, setCart, cartTotal} = useContext<any>(CartContext)
 
     return (
         <div>
+            <div>
+                <h2>Shipping & Checkout</h2>
+                <div className='h-[86vh] lg:h-[35vh] flex flex-col lg:flex-row lg:gap-x-4'>
+                    <div
+                        className='bg-green-400/20 flex-1 h-full overflow-auto lg:overflow-visible py-4 px-8 lg:py-0 lg:px-0'>
+                        <div>
+                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lf:gap-x-4'>
+                                <input type='text'
+                                       className='w-full input'
+                                       placeholder='First Name'
+                                />
+                                <input type='text'
+                                       className='w-full input'
+                                       placeholder='Last Name'
+                                />
+                            </div>
+                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lf:gap-x-4'>
+                                <input type='text'
+                                       className='w-full input'
+                                       placeholder='Phone'
+                                />
+                                <input type='text'
+                                       className='w-full input'
+                                       placeholder='Email Address'
+                                />
+                            </div>
+                        </div>
+                    </div>
 
+
+                    <div
+                        className='bg-yellow-400 flex-1 h-full lg:max-w-[40%] flex flex-col justify-between pt-3 px-8 lg:p-0'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
