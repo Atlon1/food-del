410ccc926695119fc16de2d58437245a6188ed71
Atlon1 <maciej.szajstek@gmail.com>
2023-09-14 21:34:14 +0200

    checkout preprere

--- app/components/CheckoutDetails.tsx
+++ app/components/CheckoutDetails.tsx
@@ -1,5 +1,21 @@
-const CheckoutDetails = () => {
-    return <div>CheckoutDetails</div>;
+import React, {FC, useContext} from "react";
+import {TFunc} from "ts-interface-checker";
+import Image from "next/image";
+import {CartContext} from "@/app/context/CartContext";
+
+
+interface DetailsProps {
+    setModal: React.Dispatch<React.SetStateAction<any>>
+}
+const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
+
+    const {cart, setCart, cartTotal } = useContext<any>(CartContext)
+
+    return (
+        <div>
+
+        </div>
+    )
 };
 
 export default CheckoutDetails;
