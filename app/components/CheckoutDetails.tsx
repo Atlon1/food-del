6aae89ffca9481693e8f918e8c1a47940f8b8874
Atlon1 <maciej.szajstek@gmail.com>
2023-09-15 20:29:53 +0200

    checkout detial rdy

--- app/components/CheckoutDetails.tsx
+++ app/components/CheckoutDetails.tsx
@@ -12,13 +12,14 @@ const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
 
     return (
         <div>
-            <div>
-                <h2>Shipping & Checkout</h2>
+            <div className='lg:gap-x-8 h-full lg:px-12 lg:py-8'>
+                <h2 className='mb-6 text-[20px] uppercase font-extrabold text-center lg:text-left pt-6 lg:pt-0'>Shipping
+                    & Checkout</h2>
                 <div className='h-[86vh] lg:h-[35vh] flex flex-col lg:flex-row lg:gap-x-4'>
                     <div
-                        className='bg-green-400/20 flex-1 h-full overflow-auto lg:overflow-visible py-4 px-8 lg:py-0 lg:px-0'>
+                        className='flex-1 h-full overflow-auto lg:overflow-visible py-4 px-8 lg:py-0 lg:px-0'>
                         <div className='flex flex-col gap-4 h-full'>
-                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lf:gap-x-4'>
+                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lg:gap-x-4'>
                                 <input type='text'
                                        className='w-full input'
                                        placeholder='First Name'
@@ -28,7 +29,7 @@ const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
                                        placeholder='Last Name'
                                 />
                             </div>
-                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lf:gap-x-4'>
+                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lg:gap-x-4'>
                                 <input type='text'
                                        className='w-full input'
                                        placeholder='Phone'
@@ -38,7 +39,7 @@ const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
                                        placeholder='Email Address'
                                 />
                             </div>
-                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lf:gap-x-4'>
+                            <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-0 lg:gap-x-4'>
                                 <input type='text'
                                        className='w-full input'
                                        placeholder='Street Name'
@@ -48,7 +49,7 @@ const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
                                        placeholder='Street No.'
                                 />
                             </div>
-                            <div>
+                            <div className=' flex justify-between gap-x-4'>
                                 <input type='text'
                                        className='w-full input'
                                        placeholder='Block'
@@ -59,8 +60,14 @@ const CheckoutDetails: FC<DetailsProps> = ({setModal}) => {
                                 />
                                 <input type='text'
                                        className='w-full input'
-                                       placeholder='Apartment'
+                                       placeholder='Ap. No.'
                                 />
+                            </div>
+                            <div className='flex-1 h-full'>
+                                <textarea
+                                    className='textarea w-full h-full'
+                                    placeholder='Mentions (optional)'
+                                ></textarea>
                             </div>
                         </div>
                     </div>
