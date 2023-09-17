32439c1307fce24ca51e3879077cefc69263b73d
Atlon1 <maciej.szajstek@gmail.com>
2023-09-17 17:26:48 +0200

    fixed deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,14 +30,14 @@ type Children = {
 }
 
 
-export default function RootLayout({children} : Children) {
+export default function RootLayout({children,}: { children: React.ReactNode }) {
     return (
         <CartProvider>
             <html lang="en">
             <head>
                 <title>Pizza land</title>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </head>
             <body
                 suppressHydrationWarning={true}
